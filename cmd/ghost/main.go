8f40cb1c84bf579cd1b8b145e6a2dc3378346ba4
Dmitry Shurco <dmitry@werbot.com>
2023-02-11 14:47:15 +0000

    ✨ feat: New tasks and rules

--- cmd/ghost/main.go
+++ cmd/ghost/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"math/rand"
 	"time"
@@ -12,31 +11,58 @@ import (
 	"github.com/werbot/werbot/pkg/logger"
 )
 
-var log = logger.New("ghost")
-
-const (
-	// TypeUpdateMMDB is ...
-	TypeUpdateMMDB = "cron:updateMMDB"
-
-	// TypeUpdateLists is ...
-	TypeUpdateLists = "cron:updateLists"
-)
+var log logger.Logger
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	internal.LoadConfig("../../.env")
 
+	log = logger.New("ghost")
+
 	log.Info().Msg("Start ghost server")
 
-	// ----------------------------------------
+	asyncRedisConfig := asynq.RedisClientOpt{
+		Addr:     internal.GetString("REDIS_ADDR", "localhost:6379"),
+		Password: internal.GetString("REDIS_PASSWORD", "redisPassword"),
+		DB:       1,
+	}
+
+	// server ---
+	srv := asynq.NewServer(asyncRedisConfig, asynq.Config{
+		LogLevel:    asynq.FatalLevel,
+		Concurrency: 10,
+	})
+
+	mux := asynq.NewServeMux()
+	mux.HandleFunc(typeUpdateMMDB, updateMMDBTask)
+	mux.HandleFunc(typeUpdateLists, updateHAProxyLists)
+	mux.HandleFunc(typeDownloadLicense, downloadLicense)
+
+	if err := srv.Start(mux); err != nil {
+		log.Error(err).Send()
+	}
+
+	if !fsutil.FileExists(fmt.Sprintf("%s/license.key", internal.GetString("GHOST_DATA", "/data"))) {
+		client := asynq.NewClient(asyncRedisConfig)
+
+		// tasks
+		if _, err := client.Enqueue(asynq.NewTask(typeDownloadLicense, nil)); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := client.Enqueue(asynq.NewTask(typeUpdateMMDB, nil)); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := client.Enqueue(asynq.NewTask(typeUpdateLists, nil)); err != nil {
+			log.Fatal(err)
+		}
+
+		client.Close()
+	}
+
+	// scheduler ---
 	location, _ := time.LoadLocation("UTC")
-	scheduler := asynq.NewScheduler(
-		asynq.RedisClientOpt{
-			Addr:     internal.GetString("REDIS_ADDR", "localhost:6379"),
-			Password: internal.GetString("REDIS_PASSWORD", "redisPassword"),
-			DB:       1,
-		},
+	scheduler := asynq.NewScheduler(asyncRedisConfig,
 		&asynq.SchedulerOpts{
 			LogLevel: asynq.FatalLevel,
 			Location: location,
@@ -44,76 +70,16 @@ func main() {
 	)
 
 	// update MMDB database
-	if _, err := scheduler.Register("@daily", asynq.NewTask(TypeUpdateMMDB, nil)); err != nil {
+	if _, err := scheduler.Register("@daily", asynq.NewTask(typeUpdateMMDB, nil)); err != nil {
 		log.Error(err).Send()
 	}
 
 	// update HAProxy lists
-	if _, err := scheduler.Register("@weekly", asynq.NewTask(TypeUpdateMMDB, nil)); err != nil {
+	if _, err := scheduler.Register("@weekly", asynq.NewTask(typeUpdateMMDB, nil)); err != nil {
 		log.Error(err).Send()
 	}
 
-	if err := scheduler.Start(); err != nil {
-		log.Error(err).Send()
-	}
-
-	// ----------------------------------------
-	srv := asynq.NewServer(
-		asynq.RedisClientOpt{
-			Addr:     internal.GetString("REDIS_ADDR", "localhost:6379"),
-			Password: internal.GetString("REDIS_PASSWORD", "redisPassword"),
-			DB:       1,
-		},
-		asynq.Config{
-			LogLevel:    asynq.FatalLevel,
-			Concurrency: 10,
-		},
-	)
-
-	mux := asynq.NewServeMux()
-	mux.HandleFunc(TypeUpdateMMDB, downloadMMDBTask)
-	mux.HandleFunc(TypeUpdateLists, downloadHAProxyLists)
-
-	if err := srv.Run(mux); err != nil {
-		log.Error(err).Send()
-	}
-}
-
-func downloadMMDBTask(ctx context.Context, t *asynq.Task) error {
-	log.Info().Msg("download mmdb database")
-	err := fsutil.Download(
-		fmt.Sprintf("%s/GeoLite2-Country.mmdb", internal.GetString("APP_DATA_FOLDER", "/data")),
-		fmt.Sprintf("%s/GeoLite2-Country.mmdb", internal.GetString("APP_CDN", "https://cdn.werbot.com")),
-	)
-	if err != nil {
-		log.Error(err).Send()
-	}
-	return nil
-}
-
-func downloadHAProxyLists(ctx context.Context, t *asynq.Task) error {
-	log.Info().Msg("download haproxy lists")
-	lists := []string{"blacklist-agent.txt", "cloudflare-ips.txt"}
-
-	for _, list := range lists {
-		err := fsutil.Download(
-			fmt.Sprintf("%s/haproxy/%s", internal.GetString("APP_DATA_FOLDER", "/data"), list),
-			fmt.Sprintf("https://raw.githubusercontent.com/werbot/installation/main/core/haproxy/%s", list),
-		)
-		if err != nil {
-			log.Error(err).Send()
-		}
-	}
-	return nil
-}
-
-func downloadLicense() {
-	log.Info().Msg("download license")
-	err := fsutil.Download(
-		fmt.Sprintf("%s/license.key", internal.GetString("APP_DATA_FOLDER", "/data")),
-		"https://api.werbot.com/", // TODO: down license
-	)
-	if err != nil {
+	if err := scheduler.Run(); err != nil {
 		log.Error(err).Send()
 	}
 }
---
+++ cmd/ghost/task.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hibiken/asynq"
+	"github.com/werbot/werbot/internal"
+	"github.com/werbot/werbot/pkg/fsutil"
+)
+
+const (
+	typeUpdateMMDB      = "cron:updateMMDB"
+	typeUpdateLists     = "cron:updateLists"
+	typeDownloadLicense = "license:download"
+)
+
+func testTask(ctx context.Context, t *asynq.Task) error {
+	log.Info().Msg("test")
+	return nil
+}
+
+func updateMMDBTask(ctx context.Context, t *asynq.Task) error {
+	log.Info().Msg("download mmdb database")
+	err := fsutil.Download(
+		fmt.Sprintf("%s/GeoLite2-Country.mmdb", internal.GetString("GHOST_DATA", "/data")),
+		fmt.Sprintf("%s/GeoLite2-Country.mmdb", internal.GetString("INSTALL_CDN", "https://install.werbot.com")),
+	)
+	if err != nil {
+		log.Error(err).Send()
+	}
+	return nil
+}
+
+func updateHAProxyLists(ctx context.Context, t *asynq.Task) error {
+	log.Info().Msg("download haproxy lists")
+	lists := []string{"blacklist-agent.txt", "cloudflare-ips.txt"}
+
+	for _, list := range lists {
+		err := fsutil.Download(
+			fmt.Sprintf("%s/haproxy/%s", internal.GetString("GHOST_DATA", "/data"), list),
+			fmt.Sprintf("https://raw.githubusercontent.com/werbot/install.werbot.com/main/cfg/haproxy/%s", list),
+		)
+		if err != nil {
+			log.Error(err).Send()
+		}
+	}
+	return nil
+}
+
+func downloadLicense(ctx context.Context, t *asynq.Task) error {
+	log.Info().Msg("download license")
+	err := fsutil.Download(
+		fmt.Sprintf("%s/license.key", internal.GetString("GHOST_DATA", "/data")),
+		"https://api.werbot.com/", // TODO: down license
+	)
+	if err != nil {
+		log.Error(err).Send()
+	}
+	return nil
+}
