17777b3101d032fb756475eb16a28aac42655696
Dmitry Shurco <dmitry@werbot.com>
2023-07-13 19:06:40 +0000

    🎈 perf: Update maputil

--- pkg/maputil/maputil.go
+++ pkg/maputil/maputil.go
@@ -6,85 +6,102 @@ import (
 	"strings"
 )
 
-// Get value by key path. eg "top" "top.sub"
-func Get(mp map[string]any, path string) (val any) {
-	val, _ = GetByPath(path, mp)
-	return
-}
-
-// GetByPath get value by key path from a map(map[string]any)
+// GetByPath get value by key path from a map(map[string]any). eg "top" "top.sub"
 func GetByPath(path string, mp map[string]any) (val any, ok bool) {
 	if val, ok := mp[path]; ok {
 		return val, true
 	}
 
 	// no sub key
-	if len(mp) == 0 || !strings.ContainsRune(path, '.') {
+	if len(mp) == 0 || strings.IndexByte(path, '.') < 1 {
 		return nil, false
 	}
 
 	// has sub key. eg. "top.sub"
 	keys := strings.Split(path, ".")
-	topK := keys[0]
+	return GetByPathKeys(mp, keys)
+}
+
+// GetByPathKeys get value by path keys from a map(map[string]any). eg "top" "top.sub"
+func GetByPathKeys(mp map[string]any, keys []string) (val any, ok bool) {
+	kl := len(keys)
+	if kl == 0 {
+		return mp, true
+	}
 
 	// find top item data use top key
 	var item any
+
+	topK := keys[0]
 	if item, ok = mp[topK]; !ok {
 		return
 	}
 
-	for _, k := range keys[1:] {
+	// find sub item data use sub key
+	for i, k := range keys[1:] {
 		switch tData := item.(type) {
-		case map[string]string: // is simple map
+		case map[string]string: // is string map
 			if item, ok = tData[k]; !ok {
 				return
 			}
-		case map[string]any: // is map(decode from toml/json)
+		case map[string]any: // is map(decode from toml/json/yaml)
 			if item, ok = tData[k]; !ok {
 				return
 			}
-		case map[any]any: // is map(decode from yaml)
+		case map[any]any: // is map(decode from yaml.v2)
 			if item, ok = tData[k]; !ok {
 				return
 			}
-		case []any: // is a slice
-			if item, ok = getBySlice(k, tData); !ok {
-				return
+		case []map[string]any: // is an any-map slice
+			if k == "*" {
+				if kl == i+2 {
+					return tData, true
+				}
+
+				// * is not last key, find sub item data
+				sl := make([]any, 0)
+				for _, v := range tData {
+					if val, ok = GetByPathKeys(v, keys[i+2:]); ok {
+						sl = append(sl, val)
+					}
+				}
+
+				if len(sl) > 0 {
+					return sl, true
+				}
+				return nil, false
 			}
-		case []string, []int, []float32, []float64, []bool, []rune:
-			slice := reflect.ValueOf(tData)
-			sData := make([]any, slice.Len())
-			for i := 0; i < slice.Len(); i++ {
-				sData[i] = slice.Index(i).Interface()
+
+			// k is index number
+			idx, err := strconv.Atoi(k)
+			if err != nil {
+				return nil, false
 			}
-			if item, ok = getBySlice(k, sData); !ok {
-				return
+
+			if idx >= len(tData) {
+				return nil, false
 			}
-		default: // error
+			item = tData[idx]
+		default:
+			rv := reflect.ValueOf(tData)
+			// check is slice
+			if rv.Kind() == reflect.Slice {
+				i, err := strconv.Atoi(k)
+				if err != nil {
+					return nil, false
+				}
+				if i >= rv.Len() {
+					return nil, false
+				}
+
+				item = rv.Index(i).Interface()
+				continue
+			}
+
+			// as error
 			return nil, false
 		}
 	}
 
 	return item, true
-}
-
-// getBySlice is returns a value from a slice based on the provided index `k`.
-// The returned value is of the same type as the elements in the slice and a boolean value indicating success or failure.
-func getBySlice(k string, slice []any) (val any, ok bool) {
-	// Convert the index string to an int64 value
-	i, err := strconv.ParseInt(k, 10, 64)
-
-	// Return a nil value for the element and false for success if an error occurs while parsing the index string
-	if err != nil {
-		return nil, false
-	}
-
-	// Calculate the size of the slice and return a nil value for the element and false for success
-	// if the index is greater than or equal to the size of the slice
-	if size := int64(len(slice)); i >= size {
-		return nil, false
-	}
-
-	// Return the element at the given index and true for success
-	return slice[i], true
 }
