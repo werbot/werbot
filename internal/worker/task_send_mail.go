81c69da246f121f72783d858ffe7b0c7be73257d
Dmitry Shurco <dmitry@werbot.com>
2024-10-10 13:58:50 +0000

    🦄 refactor: update import paths after directory restructuring
    Renamed the mail package path to reflect its new location under core/notification/providers. This change improves the organization of notification-related code by grouping all providers under a common directory, making it easier to manage

--- internal/worker/task_send_mail.go
+++ internal/worker/task_send_mail.go
@@ -6,7 +6,7 @@ import (
 	"google.golang.org/protobuf/proto"
 
 	notificationpb "github.com/werbot/werbot/internal/core/notification/proto/notification"
-	"github.com/werbot/werbot/internal/notification/mail"
+	"github.com/werbot/werbot/internal/core/notification/providers/mail"
 	"github.com/werbot/werbot/pkg/worker"
 )
 
